80309b15a8d7745cdd55452ff19e775cf8be6dba
XanderWonder <macluab@gmail.com>
2019-01-09 10:49:28 +0000

    update on Tuesday address all the comments

--- src/main/scala/Exercise.scala
+++ src/main/scala/Exercise.scala
@@ -1,15 +1,15 @@
 class Exercise {
-  def FirstSolution(){
-    val arrayRange = (1 to 99).toArray[Int]
-    arrayRange.reverse.foreach(arrayRange => println(s"\n$arrayRange bottle of jojo on the wall\n$arrayRange bottle of jojo on the wall\ntake one down pass it around ${arrayRange - 1} bottles of jojo in the wall"))
+  def firstSolution(){
+    val arrayRange = (99 to 1 by -1).toArray[Int]
+    arrayRange.foreach(arrayRange => println(s"\n$arrayRange bottle of jojo on the wall\n$arrayRange bottle of jojo on the wall\ntake one down pass it around ${arrayRange - 1} bottles of jojo in the wall"))
   }
-  def SecondSolution(beerNum:Int){
+  def secondSolution(beerNum:Int){
     println(s"\n$beerNum bottle of JOJO on the wall\n$beerNum bottle of JOJO on the wall")
     if(beerNum == 0){
         println("NO MORE")
-    }else SecondSolution(beerNum - 1)
+    }else secondSolution(beerNum - 1)
   }
-  def DupArray(){
+  def dupArray(){
     val dupArray = Array.range(1,1000000)
     dupArray(14212) = 123412
     val place = dupArray(14212)
