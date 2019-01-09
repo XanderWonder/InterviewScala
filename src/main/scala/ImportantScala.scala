dc765a664ed8ae7092629cdf86dcb7138959102b
XanderWonder <macluab@gmail.com>
2019-01-09 09:11:29 +0000

    added important scala code that i will need to learn for wednesday

---
+++ src/main/scala/ImportantScala.scala
@@ -0,0 +1,9 @@
+class ImportantScala {
+  def ExHigherOrder(){}
+  def ExCurrying(){}
+  def ExPartialApplied(){}
+  def ExClosure(){}
+  def ExPureFunc(){}
+  def ExAnonymousFunc(){}
+  def ExLazyEvaluation(){}
+}
