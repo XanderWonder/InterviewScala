437ae753061810a8c74e13d38962fd15f1b8dfd7
XanderWonder <macluab@gmail.com>
2019-01-09 10:43:05 +0000

    added important scala code that i will need to learn for wednesday

--- src/main/scala/WedExercise.scala
+++ src/main/scala/WedExercise.scala
@@ -1,3 +1,15 @@
+import scala.io.Source
+
 class WedExercise {
-  println("Hola")
+  def BrokenKeyboard(numWord:String){
+    val fileName = Source.fromFile("C:/Users/Admin/Desktop/Scala/wordnames.txt", "UTF-8").getLines.toList
+    for(line <- fileName){
+      if(line == numWord){
+        println(line)
+      }else{}
+    }
+  }
+  def CreditCard(){
+
+  }
 }
