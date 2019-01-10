ab52e04acac28e22c9c2c80d7d6b0ced29bca485
XanderWonder <macluab@gmail.com>
2019-01-10 17:20:21 +0000

    Merge branch 'master' of https://github.com/XanderWonder/InterviewScala into Thursday
    # Conflicts:
    #	src/main/scala/Main.scala
    #	src/main/scala/WedExercise.scala
    updates on thursday with fractorials

--- src/main/scala/Fractorial.scala
+++ src/main/scala/Fractorial.scala
@@ -1,11 +1,12 @@
 class ExersiseForDays {
     def number(num:Int): Unit ={
-      var numCheck = 0
       var factorial = 1
-      while(num < factorial){
-        numCheck = factorial / num
-        factorial += 1
+      var multiplier = 1
+      while(factorial < num){
+        multiplier = multiplier + 1
+        factorial = factorial * multiplier
       }
-      if(numCheck == 1){println(s"This is a factorial of $factorial")}else{println("Nope")}
+      if(factorial == num){println(s"This is a factorial of $multiplier and $num")}
+      else{println("Nope")}
     }
 }
