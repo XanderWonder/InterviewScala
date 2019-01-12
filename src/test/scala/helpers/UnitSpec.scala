f0d23e9e29331bb332249ff98758aba24cc71c6b
XanderWonder <macluab@gmail.com>
2019-01-12 15:12:47 +0000

    fixed the Ai for the rps game for saturday

--- src/test/scala/helpers/UnitSpec.scala
+++ src/test/scala/helpers/UnitSpec.scala
@@ -2,4 +2,4 @@ package helpers
 
 import org.scalatest._
 
-abstract class UnitSpec extends FlatSpec with Matchers with OptionValues with Inside with Inspectors
+abstract class UnitSpec extends FlatSpec with Matchers with OptionValues with Inside with Inspectors with GivenWhenThen
